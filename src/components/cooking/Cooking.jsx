5aa69e0fa167b2d63cf09ee442ec6ecc98f7b91d
mahedi066 <imam25349@gmail.com>
2024-03-17 17:32:27 +0600

    total time and total calory added

--- src/components/cooking/Cooking.jsx
+++ src/components/cooking/Cooking.jsx
@@ -1,6 +1,14 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 const Cooking = ({cooking}) => {
-    console.log(cooking)
+
+ let totalcount=cooking.map(i=>i.preparing_time)
+ let time = totalcount.reduce((acc, curr) => acc + curr, 0);
+ let totalcalo=cooking.map(i=>i.calories)
+ let calo = totalcalo.reduce((acc, curr) => acc + curr, 0);
+
+//  cooking.map(i=>setTotal(i.preparing_time))
+//  console.log(parseInt(total))
     return (
         <div>
             <h3 className='text-2xl font-bold text-center pb-4 '>Currently cooking : {cooking.length} </h3>
@@ -29,6 +37,25 @@ const Cooking = ({cooking}) => {
                         
                 ))
             }
+
+           <div>
+           <div className="overflow-x-auto">
+  <table className="table">
+
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th></th>
+        <td></td>
+        <td></td>
+        <td>total time : {time} min</td>
+        <td>total calories : {calo} calories</td>
+      </tr>
+    
+    </tbody>
+  </table>
+</div>
+           </div>
             
         </div>
     );
